f68a91f6f613b997755fca0765ff90e6ba012c9c
aichaachbouk39 <32480442+aichaachbouk39@users.noreply.github.com>
2017-10-10 12:18:44 +0200

    Add files via upload

---
+++ selfCare.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Essential Watch Self-development</title>
+    <link rel="stylesheet" href="./css/normalize.css">
+    <link rel="stylesheet" href="./css/selfDevelopment.css">
+    <link rel="stylesheet" href="./css/selfCare.css">
+    <link rel="stylesheet" href="./css/font-awesome.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+
+
+
+</head>
+
+<body>
+
+
+    <?php
+    require_once './include/nav.php'
+    ?>
+
+
+
+        <main>
+
+
+           
+        </main>
+
+        <?php
+    require_once './include/footer.php'
+    ?>
+
+
+
+</body>
+
+
+
+</html>
