3cb0c221d792b32d85b60ceea6ec9bdd9a6e98c6
Noelia Corzo Crespo <n.corzo@form.willow.if3>
2017-11-28 15:59:05 +0100

    modif title

--- user.php
+++ user.php
@@ -28,7 +28,7 @@ foreach ($data_enterprises as $enterprise) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>User</title>
+    <title>Essential Watch&trade; Create Account</title>
      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="./css/normalize.css">
     <link rel="stylesheet" href="./css/nav.css">
